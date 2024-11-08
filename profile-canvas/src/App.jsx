9c8b26da2a4cd28f5f79a706d739920e6b1d5fc9
NekoSakuraLucia <mokurolucia@gmail.com>
2024-11-08 13:57:56 +0700

    เพิ่มคอมโพเนนต์สำหรับสร้าง canvas

--- profile-canvas/src/App.jsx
+++ profile-canvas/src/App.jsx
@@ -1,7 +1,12 @@
+import GithubProfileCanvas from "./components/GithubProfileCanvas"
+
 function App() {
   return (
-    <div className="text-pink-300 font-bold">
-      Profile - Canvas
+    <div className="text-pink-300 h-screen flex flex-col items-center justify-center bg-neutral-900 font-bold text-center">
+      <h1 className="text-3xl -mt-12">Github Profile Generator</h1>
+      <div className="mt-20">
+        <GithubProfileCanvas />
+      </div>
     </div>
   )
 }
---
+++ profile-canvas/src/components/GithubProfileCanvas.jsx
@@ -0,0 +1,9 @@
+const GithubProfileCanvas = () => {
+    return (
+        <div>
+            GithubProfileCanvas
+        </div>
+    )
+}
+
+export default GithubProfileCanvas;
